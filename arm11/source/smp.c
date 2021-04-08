3f5c46e55dc6aa711fe07ba165f0f0a25e29193d
Nick Desaulniers <nick.desaulniers@gmail.com>
2021-04-07 20:32:32 -0700

    Enable additional FCRAM
    The LGR models (new 3DS, new 3DS XL, new 2DS XL) all support double the
    capacity of FCRAM; 250MiB.
    
    The process in plain English:
    1. Interrupt ID #88 is enabled.
    2. The clock speed is raised to 804Mhz.
    3. The additional FCRAM and L2 cache are enabled.
    4. Delay of 403 cycles.
    5. Spin in a Wait For Interrupt (WFI) loop until the "SOC mode" changes.
    6. Acknowledge the interrupt.
    7. Clear the interrupt.
    
    Thanks to @profi200 and the open_agb_firm project which was referenced
    extensively.
    
    A change to the DTB file is necessary to inform the kernel that
    additional physical memory is available; separate from this commit.
    
    Once enabled, in userspace you can check the amount of physical memory
    available via:
    
    $ mount -t proc /proc
    $ less /proc/meminfo
    
    From here, we can enable the additional 2 LGR cores, but it's not
    required to make use of the additional FCRAM.
    
    Link: https://github.com/profi200/open_agb_firm
    Fixes https://github.com/linux-3ds/linux/issues/1
    Signed-off-by: Nick Desaulniers <nick.desaulniers@gmail.com>

---
+++ arm11/source/smp.c
@@ -0,0 +1,63 @@
+#include <stdbool.h>
+
+#include "arm.h"
+#include "cfg11.h"
+#include "gic.h"
+#include "pdn.h"
+
+static void enable_irqs(void)
+{
+	gic_enable_control();
+	gic_disable_all();
+	gic_clear_interrupt(88);
+	gic_set_prio(88, 0);
+	gic_set_target(88, 0);
+	gic_enable_interrupt(88);
+}
+
+static void enable_fcram_l2(void)
+{
+	if (is_lgr2())
+		set_pdn_lgr_cnt(0x01 | 0x100);
+	else if (is_lgr1())
+		set_pdn_lgr_cnt(0x01);
+}
+
+static void wait_cycles(unsigned short cycles)
+{
+	asm ("1:\n\t"
+	     "subs %0, %0, #2\n\t"
+	     "yield\n\t"
+	     "bhi 1b\n\t"
+	     :: "r"(cycles) : "cc");
+}
+
+static void upclock(void)
+{
+	if (is_lgr2())
+		set_pdn_lgr_socmode(0x05);
+	else if (is_lgr1())
+		set_pdn_lgr_socmode(0x03);
+
+	// Loop until the ACK bit is set.
+	do {
+		wfi();
+	} while (!(get_pdn_lgr_socmode() & (1 << 15)));
+
+	// Write it back to ACK.
+	set_pdn_lgr_socmode(get_pdn_lgr_socmode());
+}
+
+static void set_socmode(void)
+{
+	enable_irqs();
+	enable_fcram_l2();
+	wait_cycles(403);
+	upclock();
+	gic_clear_interrupt(88);
+}
+
+void enable_cores23(void)
+{
+	set_socmode();
+}
---
+++ common/pdn.h
@@ -0,0 +1,27 @@
+#ifndef _PDN_H
+#define _PDN_H
+
+// Via: https://www.3dbrew.org/wiki/PDN_Registers
+
+#define PDN_LGR_SOCMODE	0x10141300
+#define PDN_LGR_CNT	0x10141304
+
+#ifndef __ASSEMBLY__
+
+static inline short get_pdn_lgr_socmode(void)
+{
+	return *(volatile short*)PDN_LGR_SOCMODE;
+}
+
+static inline void set_pdn_lgr_socmode(short mode)
+{
+	*(volatile short*)PDN_LGR_SOCMODE = mode;
+}
+
+static inline void set_pdn_lgr_cnt(short x)
+{
+	*(volatile short*)PDN_LGR_CNT = x;
+}
+
+#endif
+#endif
