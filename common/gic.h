91caf973e05f6bb2b7b2908e5c9c9d82b9b34db7
Nick Desaulniers <nick.desaulniers@gmail.com>
2021-04-07 21:24:59 -0700

    Use GIC identifiers and value from the Linux kernel
    Link: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/include/linux/irqchip/arm-gic.h
    Signed-off-by: Nick Desaulniers <nick.desaulniers@gmail.com>

--- common/gic.h
+++ common/gic.h
@@ -1,8 +1,23 @@
 #ifndef _GIC_H
 #define _GIC_H
 
-#define GIC_CPU_REGS_BASE	(0x17E00100)
-#define GIC_DIST_REGS_BASE	(0x17E01000)
+// These are only accessible from arm11 core...
+
+#define MPCORE_PRIV_REG_BASE	(0x17E00000)
+#define GIC_CPU_REGS_BASE	(MPCORE_PRIV_REG_BASE + 0x0100)
+#define GIC_DIST_REGS_BASE	(MPCORE_PRIV_REG_BASE + 0x1000)
+
+/*
+ * Names/values via:
+ * https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/include/linux/irqchip/arm-gic.h
+ */
+
+#define GIC_CPU_CTRL		(GIC_CPU_REGS_BASE + 0x00)
+#define GIC_DIST_ENABLE_SET	(GIC_DIST_REGS_BASE + 0x100)
+#define GIC_DIST_ENABLE_CLEAR	(GIC_DIST_REGS_BASE + 0x180)
+#define GIC_DIST_PENDING_CLEAR	(GIC_DIST_REGS_BASE + 0x280)
+#define GIC_DIST_PRI		(GIC_DIST_REGS_BASE + 0x400)
+#define GIC_DIST_TARGET		(GIC_DIST_REGS_BASE + 0x800)
 
 #ifndef __ASSEMBLY__
 
