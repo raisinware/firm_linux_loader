f73c14dbea380938400f5d8ef0a1a157826bc9a8
Nick Desaulniers <nick.desaulniers@gmail.com>
2021-04-11 19:33:13 -0700

    move SCU constants to header
    Signed-off-by: Nick Desaulniers <nick.desaulniers@gmail.com>

---
+++ common/scu.h
@@ -0,0 +1,13 @@
+#ifndef _SCU_H
+#define _SCU_H
+
+#define SCU_BASE_ADDR		0x17E00000
+#define SCU_CTRL_REG		0x00
+#define SCU_INV_ALL_REG		0x0C
+#define SCU_CTRL_ENABLE		(1 << 0)
+
+#ifndef __ASSEMBLY__
+
+
+#endif
+#endif
